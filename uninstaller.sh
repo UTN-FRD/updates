73a0ff576cd43e50a3ef2988d9abec80bb940a94
Sergio Viera <sergioviera@gmail.com>
2021-09-03 02:14:13 -0300

    v1.4

---
+++ uninstaller.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# Nombre de la base de datos: inscripcionsysacad
+dbname="inscripcionsysacad"
+# Usuario de la base de datos: inscripsysacad
+dbuser="inscripsysacad"
+dbhost="localhost"
+
+read -p "Usuario MySQL [default:root]" dbroot
+if [ ! -n "$dbroot" ];
+then 
+   dbroot="root"
+fi
+
+read -p "Password de $dbroot" dbrootpass
+if [ ! -n "$dbrootpass" ];
+then 
+   dbrootpass=""
+fi
+
+echo '
+[client]
+user='$dbroot'
+password='$dbrootpass'
+
+' > xn
+#REVISAR 
+echo '
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM '"'"''$dbuser''"'"'@'"'"''$dbhost''"'"';
+DROP DATABASE IF EXISTS `'$dbname'`;
+DROP USER '"'"''$dbuser''"'"'@'"'"''$dbhost''"'"';
+FLUSH PRIVILEGES;
+
+' > dbremove.sql
+
+mysql --defaults-extra-file=xn < dbremove.sql
+
+rm -rf *
+
+echo 'Fin del proceso!'
+
